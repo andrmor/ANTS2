99710300ab791a8354cdcba2b2d313d185b1e409
Andr <andreimorozov@hotmail.com>
2020-11-26 14:37:28 +0000

    rename

---
+++ src/gui/DAWindowTools/ageodelegatewidget.cpp
@@ -0,0 +1,6 @@
+#include "ageodelegatewidget.h"
+
+AGeoDelegateWidget::AGeoDelegateWidget()
+{
+
+}
---
+++ src/gui/DAWindowTools/ageodelegatewidget.h
@@ -0,0 +1,11 @@
+#ifndef AGEODELEGATEWIDGET_H
+#define AGEODELEGATEWIDGET_H
+
+
+class AGeoDelegateWidget
+{
+public:
+    AGeoDelegateWidget();
+};
+
+#endif // AGEODELEGATEWIDGET_H
